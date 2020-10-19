4379620a25773811515622347c55579709d08b48
tepythai <thaitepy@gmail.com>
2020-10-19 12:24:40 +0900

    set up initial project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import styles from '../styles.module.css'
+import Header from '../components/header';
+import styles from '../styles/styles.module.css';
 
 export default function Home() {
   return (
     <div className={styles.hello}>
+      <Header />
       <p>Hello World</p>
     </div>
-  )
+  );
 }
